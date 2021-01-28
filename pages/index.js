8310b0971b165b745f376258c5adfaf49550eee6
lucascdornelas <50468723+lucascdornelas@users.noreply.github.com>
2021-01-28 11:29:11 -0300

    :tada: Primeira aula

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import Widget from "../src/components/Widget";
+import QuizLogo from "../src/components/QuizLogo";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import db from "../db.json";
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const backgroundImage = db.bg;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={backgroundImage}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet....</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/lucascdornelas/QuizNext-Js" />
+    </QuizBackground>
+  );
+}
